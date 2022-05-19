0c05b4147809d168c66cee44fa82c6a30bcc5c86
Pratap Nikam <pnikam@fortinet.com>
2022-05-19 15:25:51 +0530

    pmdb#20637 - enhancements to choose icons for a custom message

---
+++ widget/edit.controller.js
@@ -0,0 +1,78 @@
+'use strict';
+(function () {
+    angular
+        .module('cybersponse')
+        .controller('editCustomPicklistMessageDev100DevCtrl', editCustomPicklistMessageDev100DevCtrl);
+
+        editCustomPicklistMessageDev100DevCtrl.$inject = ['$scope', '$uibModalInstance', '$state', 'config', 'Entity', 'picklistsService', '_'];
+
+    function editCustomPicklistMessageDev100DevCtrl($scope, $uibModalInstance, $state, config, Entity, picklistsService, _) {
+        $scope.addOptions = addOptions;
+        $scope.removeOptions = removeOptions;
+        $scope.cancel = cancel;
+        $scope.save = save;
+        $scope.config = config;
+        $scope.config.picklits_mapping = $scope.config.picklits_mapping || {
+        options: []
+       };
+      
+        function _init() {
+            if ($state.params.module) {
+                loadAttributes();
+            }
+        }
+        _init();
+      
+      
+      function loadAttributes() {
+        $scope.fieldsArray = [];
+        var entity = new Entity($state.params.module);
+        entity.loadFields().then(function () {
+          $scope.fieldsArray = entity.getFormFieldsArray();
+          $scope.fieldsArray = _.filter($scope.fieldsArray, function(item){ return item.model === 'picklists'; });
+          if(config.picklistItem){
+            var selectedPicklistItem = _.find($scope.fieldsArray, function(item){
+            return item.name === config.picklistItem;
+            });
+            $scope.loadlistitem(selectedPicklistItem);
+          }
+        }
+                                );
+      }
+      $scope.loadlistitem = function (picklistItem) {
+        if($scope.config.picklits_mapping.options.length === 0){
+          $scope.config.picklits_mapping.options.push({showSpinner:true});
+        } 
+        if(picklistItem && angular.isString(picklistItem)){
+          picklistItem = _.find($scope.fieldsArray, function(item){
+            return item.name === picklistItem;
+          });
+        }
+        picklistsService.loadPicklists(picklistItem).then(function (data) {
+            $scope.listItems = data.options;
+         });
+      };
+	
+      function removeOptions(index){
+        $scope.config.picklits_mapping.options.splice(index, 1); 
+      }
+      
+      function addOptions(){
+        $scope.config.picklits_mapping.options.push({showSpinner:true});
+      }
+
+      function cancel() {
+          $uibModalInstance.dismiss('cancel');
+       }
+
+       function save() {
+         if ($scope.editPicklistMonitorForm.$invalid) {
+              $scope.editPicklistMonitorForm.$setTouched();
+              $scope.editPicklistMonitorForm.$focusOnFirstError();
+              return;
+         }
+        $uibModalInstance.close($scope.config);
+       }
+
+    }
+})();
---
+++ widget/view.controller.js
@@ -0,0 +1,70 @@
+(function () {
+    angular
+        .module('cybersponse')
+        .controller('customPicklistMessageDev100DevCtrl', customPicklistMessageDev100DevCtrl);
+        customPicklistMessageDev100DevCtrl.$inject = ['$scope', '$state', '$interval', 'Modules', 'config', 'websocketService', '$rootScope', '_'];
+    function customPicklistMessageDev100DevCtrl($scope, $state, $interval, Modules, config, websocketService, $rootScope, _ ) {
+        $scope.config = config;
+        $scope.title = '';
+        $scope.timeinterval = 0;
+        $scope.currentTheme = $rootScope.theme.id;
+        var widgetsubscription;
+   
+         function checkPicklistCondition(){
+                Modules.get({
+                    module: $state.params.module,
+                    id: $state.params.id,
+                    __selectFields: config.picklistItem
+                }
+                ).$promise.then(function (result) {
+                  	var foundPickListValue = _.find($scope.config.picklits_mapping.options, function(item){ return item.picklistValue === result[config.picklistItem].itemValue; });
+                    if (foundPickListValue) {
+                        $scope.title = foundPickListValue.picklistLoadingTitle;
+                        $scope.picklistMessageIcon = foundPickListValue.picklistMessageIcon;
+                        $scope.showSpinner = foundPickListValue.showSpinner;
+                        $scope.loading = true;
+                    } else  {
+                        $scope.title = "";
+                        $scope.picklistMessageIcon = "";
+                        $scope.showSpinner = false;
+                        $scope.loading = false;
+                    }
+                });
+         }
+
+        $scope.$on('$destroy', function () {
+            if (widgetsubscription) {
+                websocketService.unsubscribe(widgetsubscription);
+            }
+            $interval.cancel($scope.timeinterval);
+        }
+        );
+        $scope.$on('websocket:reconnect', function () {
+            
+            widgetWSSubscribe();
+        }
+        );
+        
+        function widgetWSSubscribe() {
+            if (widgetsubscription) {
+                websocketService.unsubscribe(widgetsubscription);
+            }
+            websocketService.subscribe($state.params.module, function (result) {
+                var changedAttribute;
+                widgetsubscription = result;
+                if (angular.isDefined(result.changeData)) {
+                    if (result.changeData.includes(config.picklistItem)) {
+                        changedAttribute = config.picklistItem;
+                    }
+                }
+                if (changedAttribute) {
+                   checkPicklistCondition();
+                }
+            }
+            );
+        }
+        widgetWSSubscribe();
+        checkPicklistCondition();
+    }
+}
+)();
